cfe97bc21abe15b30d2db4d61eb0169211fedaea
Ashton Fritz <55860949+acf879@users.noreply.github.com>
2023-03-31 02:42:09 -0600

    Added svd functions
    Added various SVD functions and functions to calculate error along with reconstructions of the matrix

--- CMPT384A5/a5script.py
+++ CMPT384A5/a5script.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy as sp
 
 def makeAdjMatrix(PATH: str = "boardgames_100.json"):
     """
@@ -31,11 +32,54 @@ def makeAdjMatrix(PATH: str = "boardgames_100.json"):
                 pass #print(f"ID: {endpoint} not in top 100, discarded")
     return adj_matrix
 
+def svd(A: np.ndarray):
+    """
+    This function takes in an adjacency matrix A and returns the singular value
+    decomposition of A.
+    """
+    u,s,v_t = np.linalg.svd(A)
+    s = np.diag(s)
+    return u,s,v_t
+
+def reconstructSVD(u: np.ndarray, s: np.ndarray, v_t: np.ndarray):
+    """
+    This function takes in the singular value decomposition of a matrix A and
+    returns the matrix A.
+    """
+    return np.dot(np.dot(u,s),v_t)
+
+def reconstructSVDTruncated(u: np.ndarray, s: np.ndarray, v_t: np.ndarray, k: int):
+    """
+    This function takes in the singular value decomposition of a matrix A and
+    returns the matrix A, truncated to the first k singular values.
+    """
+    return np.dot(np.dot(u[:,:k],s[:k,:k]),v_t[:k,:])
+
+def reconstruct(A: np.ndarray, k: int):
+    """
+    This function takes in an adjacency matrix A and returns the matrix A,
+    truncated to the first k singular values.
+    """
+    u,s,v_t = np.linalg.svd(A)
+    s = np.diag(s)
+    return np.dot(np.dot(u[:,:k],s[:k,:k]),v_t[:k,:])
+
+def reconstructError(A: np.ndarray, k: int):
+    """
+    This function takes in an adjacency matrix A and returns the error between
+    the original matrix A and the matrix A, truncated to the first k singular
+    values.
+    """
+    return np.linalg.norm(A - reconstruct(A, k))
 
 def main():
     PATH = "boardgames_40.json"
     adj_matrix = makeAdjMatrix(PATH)
     print(adj_matrix)
+
+    u,s,v_t = svd(adj_matrix)
+    print("\n", np.round(reconstructSVD(u,s,v_t), 5))
+    
 
 if __name__ == "__main__":
     main()
